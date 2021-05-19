a0feb08fd54fd3d377e1960e007a48d2ac303f8d
Mayur Patel <mayurrpatel1997@gmail.com>
2021-05-19 16:56:13 -0400

    Changes done in designing

--- app/src/main/java/com/example/mainactivity/fragment/signup.java
+++ app/src/main/java/com/example/mainactivity/fragment/signup.java
@@ -8,26 +8,21 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.mainactivity.R;
 
 
-public class LogIn extends Fragment {
+public class signup extends Fragment {
 
     NavController navController;
-    Button login_btn;
-    EditText emailTxtView, signUptxtView;
-    TextView createAcc_txt;
+    TextView txt_existing_user;
 
-    public LogIn() {
-
+    public signup() {
+        // Required empty public constructor
     }
 
     @Override
@@ -40,22 +35,19 @@ public class LogIn extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
+        return inflater.inflate(R.layout.fragment_signup, container, false);
     }
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         navController = Navigation.findNavController(getActivity(), R.id.auth_host_fragment);
-        createAcc_txt = view.findViewById(R.id.txt_register_user);
+        txt_existing_user= view.findViewById(R.id.txt_existing_user);
 
-        createAcc_txt.setOnClickListener( v -> {
-
-           navController.navigate(R.id.signup);
+        txt_existing_user.setOnClickListener(v -> {
+            navController.navigate(R.id.logIn);
         });
+
     }
-
-
 }
